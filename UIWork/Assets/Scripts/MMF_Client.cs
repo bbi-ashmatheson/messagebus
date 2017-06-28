cca6b057e6bd8fbadbdc0e2df902fb259063beb0
Ash Matheson <nuclearfossil@hotmail.com>
2017-06-27 17:43:03 -0700

    testbed for memory mapped files

---
+++ UIWork/Assets/Scripts/MMF_Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public enum Mode
+    {
+        Unused = 0,
+        Client = 1,
+        Server = 2
+    }
+
+    [Serializable]
+    public class SampleData
+    {
+        public Mode Destination { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class MMF_Client : MonoBehaviour
+    {
+        private SharedMemory.CircularBuffer mReadBuffer;
+        private SharedMemory.CircularBuffer mWriteBuffer;
+        private long counter = 0;
+        void Start()
+        {
+            mReadBuffer = new SharedMemory.CircularBuffer("Test");
+            mWriteBuffer = new SharedMemory.CircularBuffer("TestReader");
+        }
+
+        void Update()
+        {
+            byte[] readBuffer = new byte[1024];
+            if (mReadBuffer.Read(readBuffer, 0, 0) > 0)
+            {
+                string result;
+                BinaryFormatter formatter = new BinaryFormatter();
+                MemoryStream memoryStream = new MemoryStream(readBuffer);
+                result = formatter.Deserialize(memoryStream) as string;
+                Debug.Log(string.Format("Found message! {0}", result));
+            }
+
+            if (Input.GetKeyUp(KeyCode.T))
+            {
+                byte[] writeBuffer = new byte[1024];
+                string message = string.Format("Test Message {0}", counter++);
+                BinaryFormatter formatter = new BinaryFormatter();
+                MemoryStream memoryStream = new MemoryStream();
+                formatter.Serialize(memoryStream, message);
+                writeBuffer = memoryStream.ToArray();
+                mWriteBuffer.Write(writeBuffer);
+
+            }
+        }
+    }
+}
---
+++ source/MMF_Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MMF_Server
+{
+    public enum Mode
+    {
+        Unused = 0,
+        Client = 1,
+        Server = 2
+    }
+
+    [Serializable]
+    public class SampleData
+    {
+        public Mode Destination { get; set; }
+        public string Message { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleData data = new SampleData()
+            {
+                Destination = Mode.Client,
+                Message = "From Server"
+            };
+
+            byte[] writeBuffer = new byte[1024];
+            byte[] readBuffer = new byte[1024];
+            SharedMemory.CircularBuffer sendRing = new SharedMemory.CircularBuffer("Test", 50, writeBuffer.Length);
+            SharedMemory.CircularBuffer readRing = new SharedMemory.CircularBuffer("TestReader", 50, writeBuffer.Length);
+            long counter = 0;
+            do
+            {
+                {
+                    string message = string.Format("Test Message {0}", counter++);
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    MemoryStream memoryStream = new MemoryStream();
+                    formatter.Serialize(memoryStream, message);
+                    writeBuffer = memoryStream.ToArray();
+                    sendRing.Write(writeBuffer);
+                }
+
+                if (readRing.Read(readBuffer) > 0)
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    MemoryStream memoryStream = new MemoryStream(readBuffer);
+                    string readData = formatter.Deserialize(memoryStream) as string;
+                    Console.WriteLine(string.Format("Found message: {0}", readData));
+                }
+            } while (true);
+        }
+    }
+}
